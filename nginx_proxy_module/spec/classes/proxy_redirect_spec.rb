ecc094e5c45672570a337526c3d7759670020be2
dcarmal <dcarmal@gmail.com>
2023-07-12 17:55:16 +0200

    Use of hiera lookup on rspec is deprecated - modified accordingly

--- nginx_proxy_module/spec/classes/proxy_redirect_spec.rb
+++ nginx_proxy_module/spec/classes/proxy_redirect_spec.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
+external_data = {
+  'proxy1' => [
+    ['https://domain.com', '10.10.10.10'],
+  ],
+  'proxy2' => [
+    ['https://domain.com/resource2', '20.20.20.20'],
+  ],
+}
+
 require 'spec_helper'
 
 describe 'nginx_proxy_module::proxy_redirect' do
-  # hiera = RSpec::Puppet::Hiera.new
-  let(:hiera_data) do
-    RSpec::Puppet::Support::Hiera.lookup('nginx_proxy_module::proxy_redirect::redirects')
-  end
-
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
@@ -15,7 +19,7 @@ describe 'nginx_proxy_module::proxy_redirect' do
       it { is_expected.to compile.with_all_deps }
 
       it 'expected to contain a notice with the expected text' do
-        hiera_data.each do |proxy_name, proxy_data|
+        external_data.each do |proxy_name, proxy_data|
           origin = proxy_data[0][0]
           target = proxy_data[0][1]
           is_expected.to contain_notify("Servidor #{proxy_name} - Encontrado origen: #{origin} apuntando a destino #{target}")
