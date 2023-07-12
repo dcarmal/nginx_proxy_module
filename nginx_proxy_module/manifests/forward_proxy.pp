8bd791997cc8ca458aae972f81c7991d337256fb
dcarmal <dcarmal@gmail.com>
2023-07-12 22:28:17 +0200

    forward_proxy class updated

--- nginx_proxy_module/manifests/forward_proxy.pp
+++ nginx_proxy_module/manifests/forward_proxy.pp
@@ -9,4 +9,15 @@
 # @example
 #   include nginx_proxy_module::forward_proxy
 class nginx_proxy_module::forward_proxy {
+  include nginx
+  nginx::resource::server { 'forward_proxy':
+    listen_port         => 80,
+    proxy               => 'http://$http_host$request_uri',
+    proxy_set_header    => ['X-Real-IP $remote_addr'],
+    access_log          => '/var/log/nginx/forward_proxy.access.log',
+    error_log           => '/var/log/nginx/forward_proxy.error.log',
+    location_cfg_append => {
+      'proxy_pass' => 'http://$host$request_uri',
+    },
+  }
 }
--- nginx_proxy_module/spec/classes/forward_proxy_spec.rb
+++ nginx_proxy_module/spec/classes/forward_proxy_spec.rb
@@ -8,6 +8,19 @@ describe 'nginx_proxy_module::forward_proxy' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
+
+      it {
+        is_expected.to contain_nginx__resource__server('forward_proxy').with(
+          'listen_port'         => 80,
+          'proxy'               => 'http://$http_host$request_uri',
+          'proxy_set_header'    => ['X-Real-IP $remote_addr'],
+          'access_log'          => '/var/log/nginx/forward_proxy.access.log',
+          'error_log'           => '/var/log/nginx/forward_proxy.error.log',
+          'location_cfg_append' => {
+            'proxy_pass' => 'http://$host$request_uri',
+          },
+        )
+      }
     end
   end
 end
